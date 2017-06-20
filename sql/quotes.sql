8a0515223a9781312d4fbdd538e77cbda058f138
yiwensong.com <yiwensong.com@gmail.com>
2017-06-20 00:08:00 +0000

    sql for adding a quotes table

---
+++ sql/quotes.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS rawdata.quotes;
+CREATE TABLE rawdata.quotes (
+  sequence BIGINT NOT NULL,
+  time TIMESTAMPTZ NOT NULL,
+  bid_price FLOAT NOT NULL,
+  bid_size FLOAT NOT NULL,
+  bid_orders INT,
+  ask_price FLOAT NOT NULL,
+  ask_size FLOAT NOT NULL,
+  ask_orders INT,
+  product_id CHAR(10) NOT NULL,
+  exchange CHAR(20) NOT NULL,
+  PRIMARY KEY(time, product_id, exchange)
+);
+GRANT SELECT ON rawdata.quotes TO public;
+GRANT INSERT ON rawdata.quotes TO gdax_md;
+
